12619193e3bb169874ca1f41dff6257fbf5b8f29
NayaPattillo <nayapattillo@gmail.com>
2023-01-12 14:46:30 -0700

    w02

---
+++ lesson2/scripts/script.js
@@ -0,0 +1,7 @@
+
+let year = document.querySelector("#year");
+year.innerHTML = new Date().getFullYear();
+
+let lastUpdate = document.querySelector("#lastUpdate");
+lastUpdate.innerHTML = new Date(document.lastModified);
+

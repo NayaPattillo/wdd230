1400228bfd53ba2d13e0f2c005d8bd2eb0d1b137
NayaPattillo <nayapattillo@gmail.com>
2023-02-15 15:13:03 -0700

    w07 assignment

---
+++ chamber/scripts/discoverScripts.js
@@ -0,0 +1,24 @@
+const year = document.querySelector("#year");
+year.textContent = new Date().getFullYear();
+
+const lastModified = document.querySelector("#lastUpdate");
+lastModified.textContent = new Date(document.lastModified).toLocaleString();
+
+
+const userVisits = document.querySelector('#userVisits');
+/*let daysBetween = new Date().getDay - new Date(document.lastModified);*/
+function daysBetween() {
+    let day = new Date().getDay();
+    localStorage.setItem('dayVisited', day)
+    let localDay = localStorage.getItem('dayVisited');
+
+    let lastVisit = new Date(document.lastModified).getDay();
+    localStorage.setItem('lastVisited', lastVisit)
+    let localVisit = localStorage.getItem('lastVisited');
+
+    let daysBetween = localDay - localVisit;
+    let rounded = Math.round(daysBetween);
+    return rounded
+}
+
+userVisits.textContent = daysBetween();

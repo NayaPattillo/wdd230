f6d2d03bc54027da1024c3b63449cc6e5351441f
NayaPattillo <nayapattillo@gmail.com>
2023-03-28 17:34:18 -0600

    bountiful foods day1

---
+++ bountifulFoods/scripts/scripts.js
@@ -0,0 +1,69 @@
+//slides
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+
+
+
+//weather
+
+const currentTemp = document.querySelector('#temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const condition = document.querySelector('#condition');
+const humidityDesc = document.querySelector('#humidity');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad,us&units=imperial&appid=408959da960ba69cf5ce9558b4e2d4dd';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok){
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      }else{
+          throw Error(await response.text());
+      }
+    } 
+    catch (error) {
+        console.log(error);
+    }
+}
+apiFetch();
+
+function displayResults(weatherData){
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(2)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    const humid = weatherData.humidity;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    condition.textContent = desc;
+    humidityDesc.textContent = humid;
+}
+
+
+//footer
+const year = document.querySelector(".year");
+year.textContent = new Date().getFullYear();
+
+
+const lastModified = document.querySelector(".lastUpdate");
+lastModified.textContent = new Date(document.lastModified).toLocaleString();
